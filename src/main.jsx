98bc51041673187bb31a8d9c7894b1df0d79434a
Michael Renolayan <mrenolayan@macbookpro.lan>
2026-06-18 16:20:33 -0700

    Add generic client slot: slot-2 (PLAN_ID: client-slot-2)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import HyroxTrainer from './HyroxTrainer.jsx'
+import HyroxTrainer from './HyroxClientSlot2.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
